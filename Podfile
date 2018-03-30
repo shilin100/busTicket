af65a4ba7974bdf9cfa852f004f53348709f3b07
madeinh <1193540803@qq.com>
2018-03-30 13:42:45 +0800

    适配

--- Podfile
+++ Podfile
@@ -1,6 +1,6 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
-
+ platform :ios, '9.0'
+source 'https://github.com/CocoaPods/Specs.git'
 target 'busTicket' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
@@ -19,7 +19,7 @@ pod 'Moya/RxSwift'
 pod 'SVProgressHUD'
 pod 'MBProgressHUD'
 pod 'TZImagePickerController'
-pod 'SnapKit'
+pod 'SnapKit', '~> 3.0.2'
 #pod 'Masonry'
 pod 'MarqueeLabel/Swift'
 pod 'MJRefresh'
@@ -38,12 +38,13 @@ pod 'RxOptional'
 # Misc
 pod 'JPush'
 pod 'IQKeyboardManagerSwift'
-pod 'ZBarSDK'
+pod 'ZBarSDK' #二维码扫描
 pod 'BaiduMapKit' #百度地图SDK
 pod 'Bugly'
 pod 'CYLTabBarController'
 pod 'ObjectMapper'
 pod 'Then'
+pod 'CGFloatLiteral', '~> 0.2'
 
 
 
@@ -58,5 +59,12 @@ pod 'Then'
     inherit! :search_paths
     # Pods for testing
   end
+end
+  post_install do |installer|
+      installer.pods_project.targets.each do |target|
+          target.build_configurations.each do |config|
+              config.build_settings['SWIFT_VERSION'] = '3.2'
+          end
+      end
 
 end
--- busTicket/ViewController.swift
+++ busTicket/ViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
+import Moya
+import Then
+import SnapKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    var text1 :UITextField!
+    var text2 :UITextField!
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        text1 = UITextField().then{
+            
+            $0.attributedPlaceholder = NSAttributedString(string: "type1")
+            $0.borderStyle = .roundedRect
+            view.addSubview($0)
+            $0.snp.makeConstraints({ (temp) in
+                temp.top.equalToSuperview().offset(200)
+                temp.left.equalToSuperview().offset(20)
+                temp.right.equalToSuperview().offset(-20)
+                temp.height.equalTo(50)
+
+            })
+            
+            
+        }
+
+        text2 = UITextField().then{
+            
+            $0.attributedPlaceholder = NSAttributedString(string: "type2")
+            $0.borderStyle = .roundedRect
+            view.addSubview($0)
+            $0.snp.makeConstraints({ (temp) in
+                temp.top.equalToSuperview().offset(400)
+                temp.left.equalToSuperview().offset(20)
+                temp.right.equalToSuperview().offset(-20)
+                temp.height.equalTo(50)
+
+            })
+            
+            
+        }
+
     }
 
     
